8c842fab7fdc21953e4da3c192309c626a68a548
bhanu-gupta <guptabhanu90@gmail.com>
2019-05-21 08:54:20 -0400

    add homework

---
+++ W7D1/job_listings/frontend/actions.js
@@ -0,0 +1,12 @@
+const selectLocation = (city, jobs) => {
+    return ({
+        type: "SWITCH_LOCATION",
+        city: city,
+        jobs: jobs
+    });
+
+}
+
+window.selectLocation = selectLocation;
+
+export default selectLocation
